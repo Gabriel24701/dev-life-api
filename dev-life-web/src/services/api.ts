f8457ad7880066bf20d645627cd47ab9d61ff69a
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-04-05 10:59:25 -0300

    feat: Adicionando a funcionalidade do Hot Reload na pagina.

--- dev-life-web/src/services/api.ts
+++ dev-life-web/src/services/api.ts
@@ -1,24 +1,21 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = {
-  // Tasks
   getTasks: async () => {
-    const res = await fetch(`${API_URL}/tasks`);
+    const res = await fetch(`${API_URL}/tasks`, { cache: 'no-store' });
     return res.json();
   },
   concludeTask: async (id: number) => {
     await fetch(`${API_URL}/tasks/${id}`, { method: 'PATCH' });
   },
   
-  // Courses
   getCourses: async () => {
-    const res = await fetch(`${API_URL}/courses`);
+    const res = await fetch(`${API_URL}/courses`, { cache: 'no-store' });
     return res.json();
   },
   
-  // Habits
   getHabits: async () => {
-    const res = await fetch(`${API_URL}/habits`);
+    const res = await fetch(`${API_URL}/habits`, { cache: 'no-store' });
     return res.json();
   }
 };
