d475c61575110facaa7cbd18fb83d586eca4c26a
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-04-05 11:23:59 -0300

    fix: Resolve erros de linting no next config usando eslint-disable

--- dev-life-web/next.config.ts
+++ dev-life-web/next.config.ts
@@ -1,13 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpack: (config) => {
+const nextConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (config: any) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
     };
     return config;
   },
+  
+  turbopack: {},
 };
 
 export default nextConfig;
