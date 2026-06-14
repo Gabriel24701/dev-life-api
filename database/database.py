ac35d196001011d1fb5ececc0900e463813aa213
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-06-14 18:21:46 -0300

    fix(db): configura api para ler url do postgresql em ambiente de producao

--- database/database.py
+++ database/database.py
@@ -1,4 +1,26 @@
-import sqlite3
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./devlife_local.db")
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
-    return sqlite3.connect("database/devlife.db")
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
