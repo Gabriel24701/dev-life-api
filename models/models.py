c38bea6f286a9179020e177df794f6cb33eb88d4
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-06-14 21:18:23 -0300

    feat(auth): adiciona dependencias jwt/bcrypt e cria tabela de usuarios

--- models/models.py
+++ models/models.py
@@ -1,11 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
 from datetime import datetime, timezone
-from database import Base
-
-
-# ==========================================
-# MODELO DE HÁBITOS
-# ==========================================
+from database.database import Base
 class Habit(Base):
     __tablename__ = "habits"
 
@@ -15,9 +11,15 @@ class Habit(Base):
     streak = Column(Integer, default=0)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
-# ==========================================
-# MODELO DE TAREFAS
-# ==========================================
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 class Task(Base):
     __tablename__ = "tasks"
 
